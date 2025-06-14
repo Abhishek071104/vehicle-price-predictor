d7a0ff241b241d1839d4ecf72a8d7a8a4d969f58
M ABHISHEK <167575384+Abhishek071104@users.noreply.github.com>
2025-06-15 01:18:07 +0530

    Update app.py

--- app.py
+++ app.py
@@ -116,12 +116,21 @@ with tab1:
             "interior_color": encode_input(interior_color, "interior_color"),
             "drivetrain": encode_input(drivetrain, "drivetrain")
         }
+        # Show progress bar while "loading"
+        progress_text = "Predicting vehicle price..."
+        my_bar = st.progress(0, text=progress_text)
+        for percent_complete in range(100):
+            time.sleep(0.01)
+            my_bar.progress(percent_complete + 1, text=progress_text)
+
+        input_df = pd.DataFrame([input_dict])
+        prediction = model.predict(input_df)[0]
+        price = int(prediction)
 
         input_df = pd.DataFrame([input_dict])
         prediction = model.predict(input_df)[0]
 
         st.success(f"💵 Estimated Price: **${int(prediction):,}**")
-        st.balloons()
 
         display_data = {
             "Make": make,
